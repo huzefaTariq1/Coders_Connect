f85ec6285511be04db59089d8715f318ba9f2174
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-08-22 19:55:34 +0500

    update user.js / add feature user registar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator')
-
+const User=require('../../models/Users')
+const gravatar=require('gravatar')
+const bcrypt=require('bcryptjs')
+const { findOne } = require('../../models/Users')
 const router = express.Router()
 
 // @route     /api/users
@@ -11,12 +14,56 @@ router.post('/',
   body('name').not().isEmpty(),
   body('email').isEmail(),
   body('password').isLength({min:6})
-], (req, res) => {
-    const error=validationResult(req)
+], async (req, res) => {
+    const error=validationResult(req);
     if(!error.isEmpty()){
-        return res.status(400).json({errors:error.array()})
+        return res.status(400).json({errors:error.array()});
     }
-    res.send('api running')
+     
+    const {name,email,password}=req.body
+    try {
+      // check user already exist
+      let user=await User.findOne({email});
+      if(user){
+        res.status(400).json({errors:[{msg:"user alrady exists"}]});
+      }
+
+
+      // get avatar
+     const avatar=gravatar.url(email,{
+      s:'200',
+      r:'pg',
+      d:'mm'
+     });
+
+   
+     
+
+     user=new User({
+      name,
+      email,
+      password,
+      avatar
+     });
+
+
+
+
+     // encrypt password
+     const salt=await bcrypt.genSalt(10);
+
+     user.password=await bcrypt.hash(password,salt)
+
+     const createdUser=await user.save()
+
+     res.status(200).send(createdUser)
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error')
+    }
+   
+
 })
 
 module.exports = router

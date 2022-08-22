80f39ebb8cfd8625000682d119a94768c72d4dae
HuzefaTariq <101399969+huzefaTariq1@users.noreply.github.com>
2022-08-22 11:00:14 +0500

    add user validation with express handler

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
-const express=require('express')
+const express = require('express')
+const { body, validationResult } = require('express-validator')
 
-const router=express.Router()
+const router = express.Router()
 
 // @route     /api/users
 // @desc      testing route
 // access     public
-router.get('/',(req,res)=>res.send('checling user route'))
+router.post('/', 
+[
+  body('name').not().isEmpty(),
+  body('email').isEmail(),
+  body('password').isLength({min:6})
+], (req, res) => {
+    const error=validationResult(req)
+    if(!error.isEmpty()){
+        return res.status(400).json({errors:error.array()})
+    }
+    res.send('api running')
+})
 
-module.exports=router
+module.exports = router
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const app=express()
 
 connectDB()
 
+// inii middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 // defining routes
 app.use('/api/users',userRoute)
